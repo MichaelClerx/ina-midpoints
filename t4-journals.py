1bf3a527b87495bc5f2602c064fb1f59d3b4ec79
Michael Clerx <michael.clerx@nottingham.ac.uk>
2024-06-06 16:57:50 +0100

    Added journal list

--- t4-journals.py
+++ t4-journals.py
@@ -11,9 +11,13 @@ w = 'where cell != "Oocyte"' if nooocytes else ''
 
 with base.connect() as con:
     c = con.cursor()
-    q = ('select journal, COUNT(pub) from midpoints_wt inner join publication'
+    q = ('select journal, COUNT(pub) as c'
+         ' from midpoints_wt inner join publication'
          f' on midpoints_wt.pub = publication.key {w}'
-         ' group by publication.journal order by publication.journal')
+         ' group by publication.journal'
+         #' order by publication.journal'
+         ' order by c desc'
+    )
     for row in c.execute(q):
         print(list(row))
 
